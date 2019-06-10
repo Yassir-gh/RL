c8f4b2d17b2b8e509c6c472f98cfc2b6f63fd549
Yassir <yassirmg@gmail.com>
2019-06-10 22:19:17 +0200

    Ajout de la version Java

---
+++ JavaVersion/Stage3A_JavaVersion/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import joinery.DataFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> columns= new ArrayList<>();
+		columns.add("a");
+		columns.add("b");
+		DataFrame<Object> df = new DataFrame<>(columns);
+		List<String> row= new ArrayList<>();
+		row.add("10");
+		row.add("20");
+		df.append(row);
+		df.append(row);
+		System.out.println(df);
+
+	}
+
+}
